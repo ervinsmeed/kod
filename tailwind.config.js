90f46bd39b3189cb07979a120ffbbe6ccd1fb204
Goku <ervinsmeed@gmail.com>
2026-02-16 18:15:27 +0600

    save

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
     extend: {
       colors: {
         "custom-dark": "#0b1220", // каалаган кара түс
+        "energy-blue": "#1f6bff",
+        "vibrant-orange": "#ff4d00",
+        "neon-lime": "#b7ff00",
       },
     },
   },
